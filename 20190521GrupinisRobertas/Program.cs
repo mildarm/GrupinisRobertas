75bbb42ad79176618c2e6dd9f261d464fd2a5217
Marius <35076113+MuchName@users.noreply.github.com>
2019-05-21 20:49:15 +0300

    kompiuteris

--- 20190521GrupinisRobertas/Program.cs
+++ 20190521GrupinisRobertas/Program.cs
@@ -40,23 +40,6 @@ namespace _20190521GrupinisRobertas
 
     }
 
-    public class Kompiuteris
-    {
-        private IDuombaze duombaze;
-
-        public Kompiuteris(IDuombaze dbaze)
-        {
-            duombaze = dbaze;
-        }
-
-        public void Veiksmas()
-        {
-            duombaze.GautiDuomenis();
-            duombaze.SiustiDuomenis();
-            duombaze.DuomenuTipas();
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
