70e25f647960bd5676554dc76e384e749689cc53
Marius <35076113+MuchName@users.noreply.github.com>
2019-05-21 20:42:10 +0300

    Update Program.cs

--- 20190521GrupinisRobertas/Program.cs
+++ 20190521GrupinisRobertas/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _20190521GrupinisRobertas
 {
-    interface IDuombaze
+    public interface IDuombaze
     {
         void GautiDuomenis(); // Gauna duomenis is DB
         void SiustiDuomenis();// Siuncia i DB
@@ -19,14 +19,49 @@ namespace _20190521GrupinisRobertas
         void SiustiDuomenis();// Siuncia i DB
         void DuomenuTipas();// 
     }
-          
 
-   
+    class CsvFailas : IDuombaze
+    {
+        public void DuomenuTipas()
+        {
+            Console.WriteLine(".csv failas");
+        }
+
+        public void GautiDuomenis()
+        {
+            Console.WriteLine("Siunciu duomenis");
+        }
+
+        public void SiustiDuomenis()
+        {
+            Console.WriteLine("Gaunu duomenis");
+        }
+
+    }
+
+    public class Kompiuteris
+    {
+        private IDuombaze duombaze;
+
+        public Kompiuteris(IDuombaze dbaze)
+        {
+            duombaze = dbaze;
+        }
+
+        public void Veiksmas()
+        {
+            duombaze.GautiDuomenis();
+            duombaze.SiustiDuomenis();
+            duombaze.DuomenuTipas();
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Kompiuteris pc = new Kompiuteris(new CsvFailas());
+            pc.Veiksmas();
         }
     }
 }
