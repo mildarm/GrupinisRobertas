c82ef335deed088fe47030a4a1aa45121f3d0f94
Marius <35076113+MuchName@users.noreply.github.com>
2019-05-21 20:25:54 +0300

    commit

---
+++ 20190521GrupinisRobertas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190521GrupinisRobertas
+{
+    interface IDuombaze
+    {
+        void GautiDuomenis(); // Gauna duomenis is DB
+        void SiustiDuomenis();// Siuncia i DB
+        void DuomenuTipas();// 
+    }
+
+    interface IRobertas
+    {
+        void GautiDuomenis(); // Gauna duomenis is DB
+        void SiustiDuomenis();// Siuncia i DB
+        void DuomenuTipas();// 
+    }
+          
+
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
