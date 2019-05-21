2f096f3e4b2a060b2aa468da5ec770b339c3f5c8
Kazimieras Padvelskis <9186@university.local>
2019-05-21 21:22:07 +0300

    pjaustykle

---
+++ 20190521GrupinisRobertas/Pjaustykle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190521GrupinisRobertas
+{
+    class Pjaustykle: IRobertas
+    {
+        public int Properties { get; set; }
+
+        public void DuomenuTipas()
+        {
+            Console.WriteLine("Pjaustykle");
+        }
+
+        public void GautiDuomenis()
+        {
+            Console.WriteLine("Gaunami duomenys");
+        }
+
+        public void SiustiDuomenis()
+        {
+            Console.WriteLine("Siunciami duomenys");
+        }
+    }
+}
