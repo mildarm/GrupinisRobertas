d1dc1d9d952bec6ee9db7c547a8d9a45eb3ed65f
Marius <35076113+MuchName@users.noreply.github.com>
2019-05-21 21:07:06 +0300

    Revert "kompiuteris"
    This reverts commit 75bbb42ad79176618c2e6dd9f261d464fd2a5217.

--- 20190521GrupinisRobertas/Program.cs
+++ 20190521GrupinisRobertas/Program.cs
@@ -40,6 +40,23 @@ namespace _20190521GrupinisRobertas
 
     }
 
+    public class Kompiuteris
+    {
+        private IDuombaze duombaze;
+
+        public Kompiuteris(IDuombaze dbaze)
+        {
+            duombaze = dbaze;
+        }
+
+        public void Veiksmas()
+        {
+            duombaze.GautiDuomenis();
+            duombaze.SiustiDuomenis();
+            duombaze.DuomenuTipas();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
